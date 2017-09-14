b7ce91b13783a6b0a93dc285f7ef4aa94884c8f3
Prof <sebastien.pisak@ac-rouen.fr>
2017-09-14 20:06:12 +0200

    passage à la version supérieur de l'IDE arduino avec la bibliothèque Arduino.h

--- terminal.h
+++ terminal.h
@@ -1,19 +1,19 @@
 /*bibliotheque permettant d'envoyer des commandes ANSI au terminal afin
 de pouvoir jouer sur la mise en forme de la page affichee. */
 /*realisee par Pisak Sebastien :
-version 1.0 du 31/01/2013
+version 2.0 du 14/09/2017
 */
 
 #ifndef TERMINAL_H
 #define TERMINAL_H
-#include "WProgram.h"
+#include "Arduino.h"
 #include <stdio.h>
 
 
 void terminalInit(void);    //initialistation du terminal avec les valeurs d'attributs par defaut
 void terminalPosition (int col, int ligne);//position du curseur
 void terminalCouleur(int text,int font);//couleur du texte et du fond d'ecran
-void terminalEfface(void);
+void terminalEfface(void);				//efface l'ecran de l'hyperterminal
 
 
 #endif
